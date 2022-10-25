3c1b3ad8b7e89d5d58412ad704a42febdd30b3b7
Vladimír Nevyhoštěný <vnevyhosteny@gmail.com>
2022-10-25 05:58:28 +0200

    feat: error handling to forwading to tyntec

--- src/forward/to-tyntec/forward-to-tyntec.service.ts
+++ src/forward/to-tyntec/forward-to-tyntec.service.ts
@@ -1,5 +1,5 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { SendMessageResponse, TyntecApiService } from '../../tyntec';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
+import { ApiError, SendMessageResponse, TyntecApiService } from '../../tyntec';
 import { ToTyntecBodyDto } from './to-tyntec.dto';
 
 @Injectable()
@@ -17,7 +17,31 @@ export class ForwardToTyntecService {
         .api(tyntecApiKey)
         .messaging.sendMessage(body);
     } catch (e) {
-      throw e;
+      if (e instanceof ApiError) {
+        if (e.status === HttpStatus.FORBIDDEN) {
+          throw (new HttpException(
+            'Access to Tyntec Conversation Inbox forbidden',
+            HttpStatus.FORBIDDEN,
+          ).cause = e);
+        }
+
+        if (e.status === HttpStatus.BAD_REQUEST) {
+          throw (new HttpException(
+            'Invalid request',
+            HttpStatus.BAD_REQUEST,
+          ).cause = e);
+        }
+
+        throw (new HttpException(
+          'Unknown Error on Tyntec API',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        ).cause = e);
+      }
+
+      throw (new HttpException(
+        'Unable to reach Tyntec Conversation Inbox',
+        HttpStatus.BAD_GATEWAY,
+      ).cause = e);
     }
   }
 }
